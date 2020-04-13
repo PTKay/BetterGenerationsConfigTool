ca4d10a4258023ee0e551a1bf2965e4360f99036
PTKickass <jp_moura99@hotmail.com>
2020-04-13 18:56:42 +0100

    Added a fix to "Game files are either missing or corrupt"

--- ConfigurationTool/RegistryData.cs
+++ ConfigurationTool/RegistryData.cs
@@ -2,6 +2,7 @@
 using System.Runtime.InteropServices;
 using System.Text;
 using Microsoft.Win32;
+using System.Diagnostics;
 
 public class RegistryData
 {
@@ -10,31 +11,66 @@ public class RegistryData
 
 	public RegistryData()
 	{
+		int fixRegistry = 0;
 		this.LCID = 1033;
 		this.ReadSaveLocationFromRegistry = "";
 		RegistryKey registryKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Sega\\Sonic Generations");
-		if (registryKey != null)
+		if (registryKey == null)
 		{
-			object value = registryKey.GetValue("locale");
-			if (value != null)
+			FixRegistry(-1);
+			registryKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Sega\\Sonic Generations");
+		}
+		object value = registryKey.GetValue("locale");
+		if (value != null)
+		{
+			string a = value.ToString();
+			if (a == "1033" || a == "1040" || a == "1036" || a == "1031" || a == "3082")
 			{
-				string a = value.ToString();
-				if (a == "1033" || a == "1040" || a == "1036" || a == "1031" || a == "3082")
-				{
-					this.LCID = int.Parse(value.ToString());
-				}
-				else
-				{
-					this.LCID = 1033;
-				}
+				this.LCID = int.Parse(value.ToString());
 			}
-			StringBuilder stringBuilder = new StringBuilder(260);
-			object value2 = registryKey.GetValue("SaveLocation");
-			if (value2 == null) return;
-			RegistryData.SHGetSpecialFolderPathW(IntPtr.Zero, stringBuilder, 5, false);
-			stringBuilder.Insert(stringBuilder.Length, "\\");
-			stringBuilder.Insert(stringBuilder.Length, value2.ToString());
-			this.ReadSaveLocationFromRegistry = stringBuilder.ToString();
+			else
+			{
+				this.LCID = 1033;
+			}
+		}
+		else
+		{
+			++fixRegistry;
+			this.LCID = 1033;
+		}
+		StringBuilder stringBuilder = new StringBuilder(260);
+		object value2 = registryKey.GetValue("SaveLocation");
+		if (value2 == null)
+		{
+			++fixRegistry;
+			value2 = "My Games\\Sonic Generations\\Saved Games";
+		}
+		RegistryData.SHGetSpecialFolderPathW(IntPtr.Zero, stringBuilder, 5, false);
+		stringBuilder.Insert(stringBuilder.Length, "\\");
+		stringBuilder.Insert(stringBuilder.Length, value2.ToString());
+		this.ReadSaveLocationFromRegistry = stringBuilder.ToString();
+		FixRegistry(fixRegistry);
+	}
+
+	private void FixRegistry(int fixType)
+	{
+		if (fixType != 0)
+		{
+			
+			var psi = new ProcessStartInfo
+			{
+				FileName = "FixGensRegistry.exe",
+				Arguments = fixType.ToString(),
+				Verb = "runas"
+			};
+
+			var process = new Process
+			{
+				StartInfo = psi
+			};
+			process.Start();
+			process.WaitForExit();
+			
 		}
 	}
 
